55bc4a633ed693f2206fdf0f09ed5376f3a35181
essejose <ze_pk2@hotmail.com>
2017-06-28 21:17:23 -0300

    push item

--- app/src/main/java/com/essejose/metrosp/MainActivity.java
+++ app/src/main/java/com/essejose/metrosp/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
                         Intent telamapa = new Intent(
                                 MainActivity.this,MapaActivity.class
                         );
-                       // telamapa.putExtra("lat", item.());
+                         telamapa.putExtra("lat", item.getCor());
 
                         startActivity(telamapa);
 
